c9b80321f4f71525c3e8422abd8f40d88929e64c
MaaeYeng2517 <pattanionline@gmail.com>
2026-01-29 08:56:28 +0700

    Create Button.tsx

---
+++ app/components/Button.tsx
@@ -0,0 +1,14 @@
+export default function Button({
+  children,
+  className = "",
+  ...props
+}: any) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-xl bg-black text-white hover:bg-gray-800 transition ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
